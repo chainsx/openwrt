d469c3329e3ade24ef5700225fa910917a271369
Hsun Lai <i@chainsx.cn>
2026-05-07 17:46:27 +0800

    add wifi support, add set-mac support

--- package/kernel/linux/modules/wireless.mk
+++ package/kernel/linux/modules/wireless.mk
@@ -22,3 +22,33 @@ define KernelPackage/net-prism54/description
 endef
 
 $(eval $(call KernelPackage,net-prism54))
+
+define KernelPackage/aic8800-wlan
+  SUBMENU:=$(WIRELESS_MENU)
+  TITLE:=AIC8800 SDIO wireless module
+  KCONFIG:=CONFIG_AIC8800_WLAN_SUPPORT
+  FILES:= \
+         $(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_fdrv/aic8800_fdrv.ko \
+         $(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_bsp/aic8800_bsp.ko
+  AUTOLOAD:=$(call AutoProbe,aic8800_fdrv aic8800_bsp)
+endef
+
+define KernelPackage/aic8800-wlan/description
+  Kernel module for AIC8800 SDIO wireless module
+endef
+
+$(eval $(call KernelPackage,aic8800-wlan))
+
+define KernelPackage/aic8800-bt
+  SUBMENU:=$(WIRELESS_MENU)
+  TITLE:=AIC8800 SDIO bluetooth module
+  KCONFIG:=CONFIG_AIC8800_BTLPM_SUPPORT
+  FILES:=$(LINUX_DIR)/bsp/drivers/net/wireless/aic8800/aic8800_btlpm/aic8800_btlpm.ko
+  AUTOLOAD:=$(call AutoProbe,aic8800_btlpm)
+endef
+
+define KernelPackage/aic8800-bt/description
+  Kernel module for AIC8800 SDIO bluetooth module
+endef
+
+$(eval $(call KernelPackage,aic8800-bt))
--- target/linux/embfly/image/t536.mk
+++ target/linux/embfly/image/t536.mk
@@ -4,7 +4,7 @@ define Device/embfly_stk-t536
   DEVICE_MODEL := STK-T536
   DEVICE_DTS = allwinner/sun55i-t536-stk-t536
   DEVICE_DTS_NAME = sun55i-t536-stk-t536
-  DEVICE_PACKAGES := kmod-mac80211
+  DEVICE_PACKAGES := aic8800-firmware
   IMAGE/sysupgrade.img.gz := sunxi-uboot-img | gzip | append-metadata
 endef
 TARGET_DEVICES += embfly_stk-t536
